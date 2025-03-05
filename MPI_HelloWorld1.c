32289a10d8e08b734a63f2d220fd3201a137954b
Saptarshi Mandal <34948568+saptarshi9766@users.noreply.github.com>
2025-03-05 14:16:09 +0100

    Add files via upload
    Naive Parallalization of a Hello world text. 
    No point to point communication involved.

---
+++ MPI_HelloWorld1.c
@@ -0,0 +1,23 @@
+/*
+Author: Saptarshi Mandal
+
+Naive Parallalization. 
+No point to point communication involved. 
+
+To compile: mpicc HelloWorldMPInaive.c
+To run:     mpirun -np 4 a.out , Note: 4 is the number of processes     
+*/
+#include <stdio.h>
+#include <mpi.h>
+int main(int argc, char *argv[])
+{
+
+    int my_rank,p;     
+    MPI_Init(&argc, &argv);                                // MPI Start
+    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);                // Get the rank of the process
+    MPI_Comm_size(MPI_COMM_WORLD,&p);                      // Get number of processes
+    printf("Hello world from the process %d \n",my_rank);
+
+
+    MPI_Finalize();                                         // MPI Stop
+}
