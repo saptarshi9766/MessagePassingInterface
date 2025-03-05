4c3924069e20ef10513945dd20129cd8c5fb15e4
Saptarshi Mandal <34948568+saptarshi9766@users.noreply.github.com>
2025-03-05 14:30:47 +0100

    Update MPI_HelloWorld1.c

--- MPI_HelloWorld1.c
+++ MPI_HelloWorld1.c
@@ -5,7 +5,7 @@ Naive Parallalization.
 No point to point communication involved. 
 
 To compile: mpicc HelloWorldMPInaive.c
-To run:     mpirun -np 4 a.out , Note: 4 is the number of processes     
+To run    : mpirun -np 4 a.out , Note: 4 is the number of processes     
 */
 #include <stdio.h>
 #include <mpi.h>
@@ -14,10 +14,11 @@ int main(int argc, char *argv[])
 
     int my_rank,p;     
     MPI_Init(&argc, &argv);                                // MPI Start
+
     MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);                // Get the rank of the process
     MPI_Comm_size(MPI_COMM_WORLD,&p);                      // Get number of processes
+    
     printf("Hello world from the process %d \n",my_rank);
-
 
     MPI_Finalize();                                         // MPI Stop
 }
